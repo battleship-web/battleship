b89fbf1ea52741c1aecc05adb5cfb8ba87f15c1b
Natchapol Thanoorutpong <natchapol.thanoorutpong@gmail.com>
2023-10-11 23:13:08 +0700

    Add storybook and upgrade yarn.

---
+++ server/client/src/storybook_example/Light.stories.jsx
@@ -0,0 +1,32 @@
+import Light from "./Light";
+
+export default {
+  component: Light,
+  title: "Light",
+  tags: ["autodocs"],
+};
+
+export const Default = {};
+
+export const Green = {
+  args: {
+    variant: "green",
+  },
+};
+export const Yellow = {
+  args: {
+    variant: "yellow",
+  },
+};
+
+export const Red = {
+  args: {
+    variant: "red",
+  },
+};
+
+export const Blue = {
+  args: {
+    variant: "blue",
+  },
+};
