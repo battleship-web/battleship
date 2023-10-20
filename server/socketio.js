33f0785aef746b133cad961e4b9b28a9a148f948
Natchapol Thanoorutpong <natchapol.thanoorutpong@gmail.com>
2023-10-20 19:46:48 +0700

    Fix bug and add gameList-related stuff

--- server/socketio.js
+++ server/socketio.js
@@ -10,7 +10,7 @@ export default function (io) {
       const { username, password } = auth;
       try {
         if (username === "guest") {
-          const guestUsername = `g:${nanoid(11)}`;
+          const guestUsername = `guest:${nanoid(12)}`;
           const promise1 = redisClient.hSet(
             `socketId:${socket.id}`,
             "username",
@@ -100,18 +100,24 @@ export default function (io) {
     socket.on("clientListRequest", async () => {
       try {
         const connectedSockets = Array.from(io.sockets.sockets.keys());
-        const redisSearchPromise = connectedSockets.map((socketId) => {
+        const redisSearchPromises = connectedSockets.map((socketId) => {
           return redisClient.hGetAll(`socketId:${socketId}`);
         });
-        const result = await Promise.all(redisSearchPromise);
-        const filteredResult = result.filter((client) => {
-          return client.role !== "admin";
+        const redisSearchResults = await Promise.all(redisSearchPromises);
+        const resultsWithSocketId = connectedSockets.map((socketId, index) => {
+          return {
+            socketId: socketId,
+            data: redisSearchResults[index],
+          };
+        });
+        const filteredResult = resultsWithSocketId.filter((client) => {
+          return client.data.role !== "admin";
         });
         const clientList = filteredResult.map((client) => {
           return {
-            nickname: client.nickname,
-            username: client.username,
-            socketId: socket.id,
+            nickname: client.data.nickname,
+            username: client.data.username,
+            socketId: client.socketId,
           };
         });
 
@@ -127,11 +133,63 @@ export default function (io) {
       }
       socket.to(inviteeSocketId).emit("incomingInvite", socket.id);
     });
-    socket.on("acceptInvite", (inviterSocketId) => {
-      socket.to(inviterSocketId).emit("inviteAccepted", socket.id);
+    socket.on("acceptInvite", async (inviterSocketId) => {
+      try {
+        const gameId = nanoid(12);
+        const promise1 = redisClient.sAdd("gameList", gameId);
+        const promise2 = redisClient.hSet(
+          `game:${gameId}`,
+          "player1",
+          inviterSocketId
+        );
+        const promise3 = redisClient.hSet(
+          `game:${gameId}`,
+          "player2",
+          socket.id
+        );
+        const promise4 = redisClient.hSet(`game:${gameId}`, "player1Score", 0);
+        const promise5 = redisClient.hSet(`game:${gameId}`, "player2Score", 0);
+        await Promise.all([promise1, promise2, promise3, promise4, promise5]);
+        socket.to(inviterSocketId).emit("inviteAccepted", socket.id);
+      } catch (error) {
+        console.log(error);
+      }
     });
     socket.on("refuseInvite", (inviterSocketId) => {
       socket.to(inviterSocketId).emit("inviteRefused", socket.id);
+    });
+    socket.on("gameListRequest", async () => {
+      const redisSearchPromise = redisClient.sMembers("gameList");
+      const gameIdList = await redisSearchPromise;
+      const gameListPromises = gameIdList.map((gameId) => {
+        return redisClient.hGetAll(`game:${gameId}`);
+      });
+      const gameList = await Promise.all(gameListPromises);
+
+      const player1InfoPromises = gameList.map((game) => {
+        return redisClient.hGet(`socketId:${game.player1}`, "username");
+      });
+      const player1Info = await Promise.all(player1InfoPromises);
+
+      const player2InfoPromises = gameList.map((game) => {
+        return redisClient.hGet(`socketId:${game.player2}`, "username");
+      });
+      const player2Info = await Promise.all(player2InfoPromises);
+
+      const formattedGameList = gameList.map((game, index) => {
+        return {
+          gameId: gameIdList[index],
+          player1: {
+            username: player1Info[index],
+            score: game.player1Score,
+          },
+          player2: {
+            username: player2Info[index],
+            score: game.player2Score,
+          },
+        };
+      });
+      socket.emit("gameList", formattedGameList);
     });
   });
 }
