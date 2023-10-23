cce2384e4b60bb6f6d5638f2f06e360e801d7088
Night <ny.chayada@gmail.com>
2023-10-23 16:00:24 +0700

    gamerulePage

---
+++ server/client/src/game/battle/gamerulePage.stories.jsx
@@ -0,0 +1,13 @@
+import gamerulePage from "./gamerulePage";
+
+export default {
+  component: gamerulePage,
+  title: "gamerulePage",
+  tags: ["autodocs"],
+};
+
+export const Default = {};
+
+
+
+
