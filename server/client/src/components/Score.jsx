2c9fae4214d9e0ce071af01880cff5d6deea6888
leo <leo555pratipnatsiri@gmail.com>
2023-10-26 00:33:58 +0700

    noey update

--- server/client/src/components/Score.jsx
+++ server/client/src/components/Score.jsx
@@ -1,31 +1,33 @@
-function Score({handleClickInstruction, user, setGameStage, anaBoard}) {
+import Board from "../squarenboard/Board";
+
+function Score({handleClickInstruction, user, setGameStage, board, onClick, size, opponentboard}) {
     return (
         <main className="text-center">
-            <div className="mb-6 relative isolate overflow-hidden bg-[url(https://thumbs.dreamstime.com/b/iron-background-threadbare-rusty-steel-covering-rivet-44688853.jpg)] px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-4 lg:px-56 lg:pt-0">
-                <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-middle">
-                    <div className="  place-items-center  lg:text-middle"></div>
-                    <h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-3xl">BATTLESHIP </h1>
-                    <p className="mt-1 text-base leading-7 text-white font-bold">Timer</p>
-                    <div className="gap-x-6 mx-3 mt-3 inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">
-                    NICKNAME
+            <div className="text-center items-center mb-6 relative isolate overflow-hidden bg-[url(https://thumbs.dreamstime.com/b/iron-background-threadbare-rusty-steel-covering-rivet-44688853.jpg)] px-10 py-4 shadow-2xl sm:rounded-3xl">
+                <h1 className="mt-2 text-3xl font-bold tracking-tight text-yellow-500 sm:text-5xl">BATTLESHIP </h1>
+                <div className="mt-2 text-base leading-7 text-white font-bold bg-blue-600 rounded-md sm:text-2xl">Timer</div>
+                <div className="mt-8 text-center items-center">
+                    <div class="flex-row">
+                        <div className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-1x1 font-bold text-red-700">
+                        General opponent
+                        </div>
+                        <span className="inline-flex ml-10 mr-20 items-center rounded-md bg-red-50 px-2 py-1 text-1x1 font-bold text-red-700">
+                        POINT X
+                        </span>
+                        <span className="inline-flex ml-20 mr-10 items-center rounded-md bg-green-50 px-2 py-1 text-1x1 font-bold text-green-700">
+                        General {user.nickname}
+                        </span>
+                        <span className="inline-flex  items-center rounded-md bg-green-50 px-2 py-1 text-1x1 font-bold text-green-700">
+                        POINT X
+                        </span>
                     </div>
-                    <span className="inline-flex ml-3 mr-4 items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
-                    POINT X
-                    </span>
-                    <span className="inline-flex ml-4 mr-3 items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
-                    {user.nickname}
-                    </span>
-                    <span className="inline-flex  items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-                    POINT X
-                    </span>
-                    <br />
-                    <br />
-                    <br />
-                    <element class="square" >
-                    </element>
-                <div className="mt-6 relative isolate overflow-hidden bg-white px-8 py-16 sm:rounded-3xl">
-                    {anaBoard}
                 </div>
+                <div className="mt-2 mb-12">
+                    <div class="flex flex-row">
+                            <h1 className="mr-2"><Board board={opponentboard} onClick={onClick} size={size}/></h1>
+                            <h1 className="mt-20 text-orange-500 sm:text-8xl font-bold">vs</h1>
+                            <h1 className="ml-2"><Board board={board} onClick={onClick} size={size}/></h1>
+                    </div>
                 </div>
             </div>
             <a
