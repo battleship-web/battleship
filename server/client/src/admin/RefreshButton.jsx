efce7974deb67aa7a13445c7ea70d4c7412d76eb
SigmaP57 <6338161221@student.chula.ac.th>
2023-11-02 09:30:31 +0700

    Button

--- server/client/src/admin/RefreshButton.jsx
+++ server/client/src/admin/RefreshButton.jsx
@@ -8,11 +8,7 @@ export default function RefreshButton() {
         socket.emit("gameListRequest");
         socket.emit("allClientListRequest");
       }}
-<<<<<<< HEAD
-      className="bg-white text-green-700 border-2 border-green-300"
-=======
       className="bg-zinc-100 border-2 border-slate-400 rounded-lg p-2 text-green-700"
->>>>>>> refs/remotes/origin/main
     >
       Refresh
     </button>
--- server/client/src/admin/ResetButton.jsx
+++ server/client/src/admin/ResetButton.jsx
@@ -3,16 +3,12 @@ import { socket } from "../socket";
 export default function ResetButton({ text, options }) {
   return (
     <button
-<<<<<<< HEAD
-      onClick={socket.emit("initiateReset", options)}
-      className="bg-white text-red-700 border-2 border-red-300"
-=======
+
       onClick={() => {
         console.log(text);
         socket.emit("initiateReset", options);
       }}
       className="bg-white text-red-700 border-red-400 border-2 rounded-lg mx-2 p-1"
->>>>>>> refs/remotes/origin/main
     >
       {text}
     </button>
